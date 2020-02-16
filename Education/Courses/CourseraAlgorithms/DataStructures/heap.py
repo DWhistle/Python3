2122164a5b34ac33f97da848f0bbd3e77954f83b
Dwhistle <Kalbasa123>
2020-02-16 19:25:31 +0300

    mod

--- Education/Courses/CourseraAlgorithms/DataStructures/heap.py
+++ Education/Courses/CourseraAlgorithms/DataStructures/heap.py
@@ -1,43 +1,47 @@
 from typing import NamedTuple
 from math import log2
-from math import ceil
+from math import ceil, floor
 
-class Node(NamedTuple):
-    left: object
-    right: object
-    value: object
-    key: int
+class Node:
+    def __init__(self, key, left = None, right = None, value = None):
+        self.left = left 
+        self.right = right
+        self.value = value
+        self.key = key
+
 
 
 class Heap:
 
     def __init__(self):
         self._tree = None
-        self._elems_num = 12
+        self._elems_num = 0
 
     def insert(self, nodes):
-        pass
+        path = self.find_last().reverse()
+        node: Node = self._tree
+        for i in range(1, len(path)):
+            if path[i] % 2 == 0:
+                node = node.left
+            else:
+                node = node.right
+        print(node)
+        
 
     def __restore_heap(self):
         pass
 
     def find_last(self):
-        level  = 2 ** ceil(log2(self._elems_num))
-        self.find_last_element(self._elems_num, level)
+        return self.find_last_element(self._elems_num)
 
-    def find_last_element(self, prev, level):
-        if prev == 1 or prev == 0:
-            return prev
+    def find_last_element(self, prev):
+        if (prev <= 1):
+            return []
+        p = prev // 2 if prev % 2 == 0 else floor(prev / 2)
+        return [p] + self.find_last_element(p) 
+h = Heap()
+n = Node(1)
+n.left = Node(2)
 
-        print(prev, level)
-        print('left' if level - prev > prev / 2 else 'right')
-        prev = prev // 2
-        self.find_last_element(prev, level // 2)
-        # from_prev = self._elems_num - 2 ** level
-        # from_next = 2 ** (level + 1) - 1 - self._elems_num 
-        # print((2 ** (level + 1)) // 4)
-        # print(2 ** (level + 1) - self._elems_num)
-        # print('right' if 2 ** (level + 1) - self._elems_num > (2 ** (level + 1)) // 4 else 'left')
-        #print(level, self._elems_num - 2 ** (level + 1) )
 
-Heap().find_last()
+print(Heap().find_last())
